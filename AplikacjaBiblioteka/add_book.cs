23ee49697c974067edf0ed3cd9c27baa825ec5d6
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2020-12-29 17:47:11 +0100

    Deletion of price from book table - not needed, and added validation to data input regarding book

--- AplikacjaBiblioteka/add_book.cs
+++ AplikacjaBiblioteka/add_book.cs
@@ -22,23 +22,49 @@ namespace AplikacjaBiblioteka
 
         private void button1_Click(object sender, EventArgs e)
         {
-            con.Open();
-            //Adding new book to data base
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "insert into book_info(name, author_name, publication_name, purchase_date, price, quantity) values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + dateTimePicker1.Text + "'," + textBox5.Text + "," + textBox6.Text + ")";
-            cmd.ExecuteNonQuery();
-            con.Close();
+            int correctData = 0;
+            int quantity = 0;
+            bool canConvert = int.TryParse(textBox5.Text, out quantity);
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Podaj tytuł książki");
+            }
+            else if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Podaj autora");
+            }
+            else if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Podaj wydawnictwo");
+            }
+            else if (canConvert == false)
+            {
+                MessageBox.Show("Podaj ilość");
+            }
+            else
+            {
+                correctData = 1;
+            }
 
-            //Seting default values for all textboxes
-            textBox1.Text = "";
-            textBox2.Text = "";
-            textBox3.Text = "";
-            textBox5.Text = "";
-            textBox6.Text = "";
+            if (correctData == 1)
+            {
+                con.Open();
+                //Adding new book to data base
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into book_info(name, author_name, publication_name, purchase_date, quantity) values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + DateTime.Parse(dateTimePicker1.Text) + "'," + quantity + ")";
+                cmd.ExecuteNonQuery();
+                con.Close();
 
-            //Show info that book was added
-            MessageBox.Show("Dodano książkę");
+                //Seting default values for all textboxes
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox5.Text = "";
+
+                //Show info that book was added
+                MessageBox.Show("Dodano książkę");
+            }
         }
     }
 }

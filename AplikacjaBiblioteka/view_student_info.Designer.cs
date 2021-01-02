92cb9579032f4ef41c084b916ec26f4ff944ed95
Tomasz Jan Oksiędzki <oksiedz@gmail.com>
2021-01-02 19:22:32 +0100

    Added functionality to search through the list of students while live writting.

--- AplikacjaBiblioteka/view_student_info.Designer.cs
+++ AplikacjaBiblioteka/view_student_info.Designer.cs
@@ -30,6 +30,8 @@ namespace AplikacjaBiblioteka
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,25 +40,48 @@ namespace AplikacjaBiblioteka
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(243, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(883, 242);
+            this.dataGridView1.Size = new System.Drawing.Size(1195, 623);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(18, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wyszukiwanie studenta";
             // 
             // view_student_info
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1150, 434);
+            this.ClientSize = new System.Drawing.Size(1450, 647);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "view_student_info";
             this.Text = "view_student_info";
             this.Load += new System.EventHandler(this.view_student_info_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- AplikacjaBiblioteka/view_student_info.cs
+++ AplikacjaBiblioteka/view_student_info.cs
@@ -21,10 +21,9 @@ namespace AplikacjaBiblioteka
 
         private void view_student_info_Load(object sender, EventArgs e)
         {
-            int j = 0;
-
             try
             {
+                int j = 0;
                 if (con.State == ConnectionState.Open)
                 {
                     con.Close();
@@ -63,5 +62,49 @@ namespace AplikacjaBiblioteka
             }
         }
 
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                int j = 0;
+                dataGridView1.Columns.Clear();
+                dataGridView1.Refresh();
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select Id as Identyfikator, name as Imie_Nazwisko, index_no as Nr_Indeksu, department as Wydział, phone as Telefon, email as EMail, image as Zdjęcie from student_info where name like '%" + textBox1.Text + "%' or index_no like '%" + textBox1.Text + "%' or department like '%" + textBox1.Text + "%' or phone like '%" + textBox1.Text + "%' or email like '%" + textBox1.Text + "%'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                Bitmap img;
+                DataGridViewImageColumn imageCol = new DataGridViewImageColumn();
+                imageCol.Width = 500;
+                imageCol.HeaderText = "Podgląd zdjęcia";
+                imageCol.ImageLayout = DataGridViewImageCellLayout.Zoom;
+                imageCol.Width = 100;
+                dataGridView1.Columns.Add(imageCol);
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    img = new Bitmap(@"..\..\" + dr["Zdjęcie"].ToString());
+                    dataGridView1.Rows[j].Cells[7].Value = img;
+                    dataGridView1.Rows[j].Height = 100;
+                    j = j + 1;
+
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
     }
 }
